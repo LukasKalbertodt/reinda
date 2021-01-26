3784cf8a1aaed194e09ab0547fa6635d44d2a8ea
Lukas Kalbertodt <lukas.kalbertodt@gmail.com>
2021-01-26 13:05:59 +0100

    Add helper script to easily run all tests in all modes

---
+++ test-all.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+for flags in "" "--release" "--features=debug_is_prod"; do
+  cargo test -p reinda-core $flags
+  cargo test -p reinda-macros $flags
+  cargo test $flags
+done
